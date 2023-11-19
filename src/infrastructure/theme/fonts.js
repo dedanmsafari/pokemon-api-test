f7c8a5401c8c714d4bde45dd5ed4e35058ef40cb
dedanmsafari <dedan.msafari@gmail.com>
2023-11-19 19:08:21 +0300

    more styling

---
+++ src/infrastructure/theme/fonts.js
@@ -0,0 +1,25 @@
+export const fonts = {
+  light: "Rubik_300Light",
+  body: "Rubik_400Regular",
+  medium: "Roboto_500Medium",
+  heading: " Rubik_700Bold",
+};
+
+export const fontWeights = {
+  light: 300,
+  regular: 400,
+  medium: 500,
+  bold: 700,
+};
+
+export const fontSizes = {
+  caption: "12px",
+  button: "14px",
+  body: "16px",
+  title: "20px",
+  h5: "24px",
+  h4: "30px",
+  h3: "45px",
+  h2: "56px",
+  h1: "112px",
+};
