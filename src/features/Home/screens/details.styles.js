1e05f6295781c61a5d44ef692d036b5dd93d5bc0
dedanmsafari <dedan.msafari@gmail.com>
2023-11-19 19:07:46 +0300

    details style

---
+++ src/features/Home/screens/details.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ImageBlock = styled.View`
+  flex: 0.5;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ImageButton = styled.TouchableOpacity`
+  flex-direction: row;
+  padding: 15px;
+  justify-content: center;
+  background-color: green;
+  border-radius: 30px;
+`;
