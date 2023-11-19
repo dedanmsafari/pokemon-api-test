52c30a166632f804861f6f25cb2425e70ac11e48
dedanmsafari <dedan.msafari@gmail.com>
2023-11-19 19:08:08 +0300

    colors

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,25 @@
+export const colors = {
+  brand: {
+    primary: "#FD5E53",
+    secondary: "#78AC78",
+    // muted: "#78AC78",
+    // tertiary: "#138000",
+  },
+  ui: {
+    primary: "#46494C",
+    secondary: "#757575",
+    tertiary: "#F1F1F1",
+    quaternary: "#FFFFFF",
+    icons: "#F56E0B",
+    disabled: "#DEDEDE",
+    error: "#D0421B",
+    success: "#138000",
+    header: "#48AB48",
+    brand: "#279B48",
+  },
+  bg: {
+    primary: "#FFFFFF",
+    secondary: "#F1F1F1",
+    tertiary: "#EFF6EC",
+  },
+};
