21bf66a437413e267980558051255bfd2ebe6d5d
dedanmsafari <dedan.msafari@gmail.com>
2023-11-19 19:06:32 +0300

    main component

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useRef } from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts as useRubik,
+  Rubik_300Light,
+  Rubik_400Regular,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik";
+import {
+  useFonts as useRoboto,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from "@expo-google-fonts/roboto";
+
+import Navigation from "./src/infrastructure/navigation";
+import { theme } from "./src/infrastructure/theme";
+import { ThemeProvider } from "styled-components";
 
 export default function App() {
+  let [RubikLoaded] = useRubik({
+    Rubik_300Light,
+    Rubik_400Regular,
+    Rubik_700Bold,
+  });
+
+  let [RobotoLoaded] = useRoboto({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+
+  const navigationRef = useRef();
+  const onLayoutRootView = useCallback(async () => {
+    if (RubikLoaded && RobotoLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [RubikLoaded, RobotoLoaded]);
+
+  if (!RubikLoaded || !RobotoLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <Navigation onReady={onLayoutRootView} navigationRef={navigationRef} />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
